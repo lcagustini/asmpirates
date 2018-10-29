818bb1d8096b9d56e392fc8b6d5e1c114a017a31
lcagustini <torresmon235@gmail.com>
2018-10-29 15:32:39 -0300

    Bg scroll

--- src/main.s
+++ src/main.s
@@ -29,12 +29,12 @@
 .text
 main:
     mov r0, #DISPCNT                @Display Controller reg
-    mov r1, #0xF00                  @Mode 0 + BG0-3 enabled
+    mov r1, #0b100000000            @Mode 0 + BG0 enabled
     str r1, [r0]
 
     mov r0, #0x4000000              @Setting up BG0 with:
     add r0, #8
-    mov r1, #0b00100010000000000    @Priority = 0; TileBase = 0; Mosaic = false; palette = 16/16; MapBase = 4; Size = 256x256
+    mov r1, #0b11100010000000000    @Priority = 0; TileBase = 0; Mosaic = false; palette = 16/16; MapBase = 4; Size = 256x256
     str r1, [r0]
 
     ldr r3, =test_tile              @Tile source
@@ -47,30 +47,41 @@ main:
     mov r5, #1                      @For now needs only two colors, which is a word long for both
     dma0_copy r3, r4, r5
 
+    mov r6, #0
+    mov r7, #0
 forever:
     mov r0, #0x4000000              @r0 = KEYINPUT
     add r0, #0x130
     ldr r0, [r0]                    @Load user input
 
-/*
 input.start:
 input.right:
     and r1, r0, #0b00100000         @and "right" bit is set
     cmp r1, #0
-
+    addne r6, #1                    @if "right" scrolls bg accordingly
 input.left:
     and r1, r0, #0b00010000         @and "left" bit is set
     cmp r1, #0
+    subne r6, #1                    @if "left" scrolls bg accordingly
+
+    mov r1, #0x4000000              @Stores current x scroll to BG0 X-offset reg
+    add r1, #0x10
+    strh r6, [r1]
 
 input.up:
     and r1, r0, #0b10000000         @and "up" bit is set
     cmp r1, #0
+    subne r7, #1                    @if "up" scrolls bg accordingly
 
 input.down:
     and r1, r0, #0b01000000         @and "down" bit is set
     cmp r1, #0
+    addne r7, #1                    @if "down" scrolls bg accordingly
+
+    mov r1, #0x4000000              @Stores current y scroll to BG0 Y-offset reg
+    add r1, #0x12
+    strh r7, [r1]
 input.end:
-*/
 
 wait_vblank:
     mov r0, #0x4000000              @Loads REG_VCOUNT
